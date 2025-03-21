5b0a494d1a91b4aa80003e7485a194574c1596db
Hana <a.hana@aui.ma>
2025-03-21 14:50:07 +0000

    feat(landing): add analytics

--- apps/landing/src/routes/+layout.svelte
+++ apps/landing/src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
+	import { dev } from '$app/environment';
 	import Navbar from '$lib/components/navbar.svelte';
-import '../app.css';
+	import '../app.css';
 	let { children } = $props();
+
+	// Inject Vercel Analytics
+	injectAnalytics({
+		mode: dev ? 'development' : 'production',
+	});
 </script>
 <div class="bg-background h-screen w-screen flex flex-col" style="background: linear-gradient(90deg, #0A192F 21.7px, transparent 1%) center, linear-gradient(#0A192F 21.7px, transparent 1%) center, #00FF99C0; background-size: 22.0px 22.0px">
 	<Navbar />
