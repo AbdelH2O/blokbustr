ef50efa94618b34241e21687cd70273b4f838a3b
Hana <a.hana@aui.ma>
2025-04-19 23:29:56 +0100

    fix: use correct chain type

--- services/watcher/src/utils/redis.ts
+++ services/watcher/src/utils/redis.ts
@@ -1,9 +1,10 @@
+import { Chain } from "@blokbustr/schema";
 import { createClient, RedisClientType } from "redis";
 
 export const redisClient: RedisClientType = createClient({ url: process.env.REDIS_URL });
 await redisClient.connect();
 
-export async function isWatchedAddress(chain: string, address: string): Promise<boolean> {
+export async function isWatchedAddress(chain: Chain, address: string): Promise<boolean> {
   if (!address) return false;
   const key = `watched_addresses:${chain}`;
   return await redisClient.sIsMember(key, address.toLowerCase());
